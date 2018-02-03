0f2ac70194c1c1865d735574ea3181927d2f6568
Jim Kyle <jim@jwkyle.com>
2018-02-03 12:45:21 -0600

    inti commit

---
+++ src/crystal-test-proj.cr
@@ -0,0 +1,6 @@
+require "./crystal-test-proj/*"
+
+# TODO: Write documentation for `Crystal::Test::Proj`
+module Crystal::Test::Proj
+  # TODO: Put your code here
+end
---
+++ src/crystal-test-proj/version.cr
@@ -0,0 +1,3 @@
+module Crystal::Test::Proj
+  VERSION = "0.1.0"
+end
